6344b68c347bf8379f1da6b70fca6ae38b81e906
mky1428 <81858099+mky1428@users.noreply.github.com>
2021-09-07 10:33:40 +0900

    Update MainActivity.kt

--- app/src/main/java/com/mky1428/githubtest/MainActivity.kt
+++ app/src/main/java/com/mky1428/githubtest/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar?.title = "GitHub TEST"
+        
+        var a:Int = 10
     }
 }
